12355152cbdf7b6c72b341bfcd34e06a41ff8677
naomi <naomi.nagy01@gmail.com>
2022-12-20 11:18:29 +0200

    jsjsjsjsjsjsjsjsjs

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function home()
+{
+document.getElementById("f-page").style.visibility="visible";
+document.getElementById("w-page").style.visibility="hidden";
+}
+function work()
+{
+document.getElementById("w-page").style.visibility="visible";
+document.getElementById("f-page").style.visibility="hidden";
+}
+
+
